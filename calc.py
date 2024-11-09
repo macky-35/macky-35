639432c0469bff55265eea2b36e2904bbcdb150b
macky-35 <mackyroger.amaro@my.jru.edu>
2024-11-09 13:55:33 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
-# test_calc.py
-from calc import add, subtract
-def test_add():
-  assert add(2, 3) == 5
-def test_subtract():
-  assert subtract(5, 3) == 2
+# calc.py
+def add(a, b):
+  return a + b
+def subtract(a, b):
+  return a - b
+
+!git add calc.py
